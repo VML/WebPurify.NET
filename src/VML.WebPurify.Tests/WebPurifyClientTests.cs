c3b58bdfb72cfd2a41c19b1a5f93b87bf42c43b1
Ben Ramey <bramey@vml.com>
2014-02-10 17:07:30 -0600

    Flesh out image account, status and check methods

--- src/VML.WebPurify.Tests/WebPurifyClientTests.cs
+++ src/VML.WebPurify.Tests/WebPurifyClientTests.cs
@@ -3,7 +3,7 @@
 //   Copyright VML 2014. All rights reserved.
 //  </copyright>
 //  <created>02/10/2014 3:36 PM</created>
-//  <updated>02/10/2014 4:19 PM by Ben Ramey</updated>
+//  <updated>02/10/2014 5:04 PM by Ben Ramey</updated>
 // --------------------------------------------------------------------------------------------------------------------
 
 #region Usings
@@ -11,6 +11,9 @@
 using System;
 using System.Linq;
 using FluentAssertions;
+using NSubstitute;
+using RestSharp;
+using VML.WebPurify.Endpoints;
 using Xunit;
 using Xunit.Extensions;
 
@@ -23,6 +26,7 @@ namespace VML.WebPurify.Tests
         #region Constants and Fields
 
         private readonly WebPurifyClient _client;
+        private readonly IRestClient _restClientMock;
 
         #endregion
 
@@ -30,18 +34,15 @@ namespace VML.WebPurify.Tests
 
         public WebPurifyClientTests()
         {
-            _client = new WebPurifyClient("fake_apikey");
+            _restClientMock = Substitute.For<IRestClient>();
+            _client = new WebPurifyClient("fake_apikey", _restClientMock);
+
+            _restClientMock.BaseUrl.Returns("http://example.com");
         }
 
         #endregion
 
         #region Public Methods
-
-        [Fact]
-        public void CheckImage_NullImageUrl_Throws()
-        {
-            _client.Invoking(c => c.CheckImage(null)).ShouldThrow<ArgumentNullException>();
-        }
 
         [Theory]
         [InlineData("")]
@@ -50,6 +51,112 @@ namespace VML.WebPurify.Tests
         public void Constructor_InvalidKey_Throws(string apiKey)
         {
             Assert.Throws<ArgumentException>(() => new WebPurifyClient(apiKey));
+            Assert.Throws<ArgumentException>(() => new WebPurifyClient(new DefaultHttpEndpoints(), apiKey));
+        }
+
+        [Fact]
+        public void ImageCheck_CallsPostOnce()
+        {
+            Uri imageUri = new Uri("http://example.com");
+
+            _client.ImageCheck(imageUri);
+
+            _restClientMock.ReceivedWithAnyArgs(1).Post(new RestRequest());
+        }
+
+        [Fact]
+        public void ImageCheck_CallsPostWithCorrectParameters()
+        {
+            Uri imageUri = new Uri("http://example.com");
+
+            IRestRequest request = null;
+            _restClientMock.WhenForAnyArgs(c => c.Post(new RestRequest()))
+                           .Do(ci => request = (IRestRequest)ci.Args()[0]);
+
+            _client.ImageCheck(imageUri);
+
+            request.Should().NotBeNull();
+            request.Parameters.Should().NotBeNull();
+
+            request.Parameters.Any(p => p.Name == "api_key").Should().BeTrue();
+            request.Parameters.Any(p => p.Name == "imgurl").Should().BeTrue();
+            request.Parameters.Any(p => p.Name == "method").Should().BeTrue();
+
+            request.Parameters.First(p => p.Name == "api_key").Value.Should().Be("fake_apikey");
+            request.Parameters.First(p => p.Name == "imgurl").Value.Should().Be(imageUri.ToString());
+            request.Parameters.First(p => p.Name == "method").Value.Should().Be("webpurify.live.imgcheck");
+        }
+
+        [Fact]
+        public void ImageCheck_NullImageUrl_Throws()
+        {
+            _client.Invoking(c => c.ImageCheck(null)).ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void ImageStatus_CallsGetOnce()
+        {
+            _client.ImageStatus("fake_image_id");
+
+            _restClientMock.ReceivedWithAnyArgs(1).Get(new RestRequest());
+        }
+        [Fact]
+        public void ImageAccount_CallsGetOnce()
+        {
+            _client.ImageAccount();
+
+            _restClientMock.ReceivedWithAnyArgs(1).Get(new RestRequest());
+        }
+
+        [Fact]
+        public void ImageAccount_CallsGetWithCorrectParameters()
+        {
+            IRestRequest request = null;
+            _restClientMock.WhenForAnyArgs(c => c.Get(new RestRequest()))
+                           .Do(ci => request = (IRestRequest)ci.Args()[0]);
+
+            _client.ImageAccount();
+
+            request.Should().NotBeNull();
+            request.Parameters.Should().NotBeNull();
+
+            request.Parameters.Any(p => p.Name == "api_key").Should().BeTrue();
+            request.Parameters.Any(p => p.Name == "method").Should().BeTrue();
+
+            request.Parameters.First(p => p.Name == "api_key").Value.Should().Be("fake_apikey");
+            request.Parameters.First(p => p.Name == "method").Value.Should().Be("webpurify.live.imgaccount");
+        }
+
+        [Fact]
+        public void ImageStatus_CallsGetWithCorrectParameters()
+        {
+            string imageId = "fake_image_id";
+
+            IRestRequest request = null;
+            _restClientMock.WhenForAnyArgs(c => c.Get(new RestRequest()))
+                           .Do(ci => request = (IRestRequest)ci.Args()[0]);
+
+            _client.ImageStatus(imageId);
+
+            request.Should().NotBeNull();
+            request.Parameters.Should().NotBeNull();
+
+            request.Parameters.Any(p => p.Name == "api_key").Should().BeTrue();
+            request.Parameters.Any(p => p.Name == "imgid").Should().BeTrue();
+            request.Parameters.Any(p => p.Name == "method").Should().BeTrue();
+
+            request.Parameters.First(p => p.Name == "api_key").Value.Should().Be("fake_apikey");
+            request.Parameters.First(p => p.Name == "imgid").Value.Should().Be(imageId);
+            request.Parameters.First(p => p.Name == "method").Value.Should().Be("webpurify.live.imgstatus");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void ImageStatus_InvalidImageId_Throws(string imageId)
+        {
+            _client.Invoking(c => c.ImageStatus(imageId)).ShouldThrow<ArgumentNullException>();
         }
 
         #endregion
--- src/VML.WebPurify/WebPurifyClient.cs
+++ src/VML.WebPurify/WebPurifyClient.cs
@@ -3,7 +3,7 @@
 //   Copyright VML 2014. All rights reserved.
 //  </copyright>
 //  <created>02/10/2014 3:54 PM</created>
-//  <updated>02/10/2014 4:19 PM by Ben Ramey</updated>
+//  <updated>02/10/2014 5:06 PM by Ben Ramey</updated>
 // --------------------------------------------------------------------------------------------------------------------
 
 #define USETHROWER
@@ -12,7 +12,10 @@
 
 using System;
 using System.Linq;
+using RestSharp;
 using Thrower;
+using VML.WebPurify.Interfaces;
+using VML.WebPurify.Requests;
 
 #endregion
 
@@ -20,20 +23,81 @@ namespace VML.WebPurify
 {
     public class WebPurifyClient
     {
+        #region Constants and Fields
+
+        private readonly string _apiKey;
+        private readonly IRestClient _restClient;
+
+        #endregion
+
         #region Constructors and Destructors
 
-        public WebPurifyClient(string apiKey)
+        public WebPurifyClient(IEndpoints endpoints, string apiKey)
         {
             Raise<ArgumentException>.If(string.IsNullOrWhiteSpace(apiKey));
+
+            _restClient = new RestClient();
+            _apiKey = apiKey;
+        }
+
+        public WebPurifyClient(string apiKey)
+            : this(null, apiKey)
+        {
+        }
+
+        public WebPurifyClient(string apiKey, IRestClient restClient)
+            : this(null, apiKey)
+        {
+            _restClient = restClient;
         }
 
         #endregion
 
         #region Public Methods
 
-        public void CheckImage(Uri imageUrl)
+        public void ImageAccount()
         {
-            Raise<ArgumentNullException>.IfIsNull(imageUrl);
+            ImageAccountRequest imageAccountRequest = new ImageAccountRequest
+                {
+                    ApiKey = _apiKey
+                };
+
+            RestRequest request = new RestRequest(Method.GET);
+            request.Parameters.AddRange(imageAccountRequest.GetParameters());
+
+            _restClient.Get(request);
+        }
+
+        public void ImageCheck(Uri imageUri)
+        {
+            Raise<ArgumentNullException>.IfIsNull(imageUri);
+
+            ImageCheckRequest imageCheckRequest = new ImageCheckRequest
+                {
+                    ApiKey = _apiKey,
+                    ImageUri = imageUri
+                };
+
+            RestRequest request = new RestRequest(Method.POST);
+            request.Parameters.AddRange(imageCheckRequest.GetParameters());
+
+            _restClient.Post(request);
+        }
+
+        public void ImageStatus(string imageId)
+        {
+            Raise<ArgumentNullException>.If(string.IsNullOrWhiteSpace(imageId));
+
+            ImageStatusRequest imageStatusRequest = new ImageStatusRequest
+                {
+                    ApiKey = _apiKey,
+                    ImageId = imageId
+                };
+
+            RestRequest request = new RestRequest(Method.GET);
+            request.Parameters.AddRange(imageStatusRequest.GetParameters());
+
+            _restClient.Get(request);
         }
 
         #endregion
